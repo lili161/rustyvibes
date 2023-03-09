b03fe7cb45a7160bf2d4b987eb0d718e8d0b3d9e
LR <1617118335@qq.com>
2023-03-09 13:02:47 +0800

    msg:first runable ui

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,8 @@ mod args;
 mod keycode;
 mod play_sound;
 mod start;
-use crate::args::ArgParser;
-use clap::Parser;
-use fltk::{enums::*, prelude::*, *};
-use stop_thread::kill_thread_forcibly_exit_code;
-
-use std::{
-    collections::HashMap,
-    fs,
-    thread::{self, JoinHandle},
-};
+use fltk::{prelude::*, *};
+use std::{collections::HashMap, fs, thread::JoinHandle};
 
 const ASCII_ART: &str = r#"
 ██████  ██    ██ ███████ ████████ ██    ██ ██    ██ ██ ██████  ███████ ███████ 
@@ -20,64 +12,51 @@ const ASCII_ART: &str = r#"
 ██   ██ ██    ██      ██    ██       ██     ██  ██  ██ ██   ██ ██           ██ 
 ██   ██  ██████  ███████    ██       ██      ████   ██ ██████  ███████ ███████
 "#;
-
 fn main() {
     let app = app::App::default();
-    // =============================
-    let mut soundPacks = Vec::<String>::new();
-    let mut soundPacks_map = HashMap::<String, String>::new();
-    // println!("{}", ASCII_ART);
-    let a = ArgParser::parse();
-    // let soundpack = a.soundpack;
-    let soundpack = String::from("D:\\KeyBoardSound\\Soundpacks\\cherrymx-blue-pbt");
-    // let paths = fs::read_dir("D:\\Work\\rustyviber\\rustyvibes\\Soundpacks").unwrap();
+    let mut sound_packs = Vec::<String>::new();
+    let mut sound_packs_map = HashMap::<String, String>::new();
+    println!("{}", ASCII_ART);
     let paths = fs::read_dir(".\\Soundpacks").unwrap();
-
     for path in paths {
         let path = path.unwrap();
         let file_name = path.file_name();
+        if file_name.to_str().unwrap().starts_with("."){continue;}
         let file_path = path.path();
-        soundPacks.push(file_name.clone().into_string().unwrap());
-        soundPacks_map.insert(
+        sound_packs.push(file_name.clone().into_string().unwrap());
+        sound_packs_map.insert(
             file_name.clone().into_string().unwrap(),
             file_path.into_os_string().into_string().unwrap(),
         );
     }
-    println!("{:#?}", soundPacks_map);
-    let handle =create_service(soundpack);
-
-    // =============================
-
-
-
-
-    let mut my_window = window::Window::new(100, 100, 400, 300, "Daily Report");
-    let mut menubar = menu::MenuBar::new(0, 0, 400, 40, "1");
+    let sound_packs_copy = sound_packs.clone();
+    let sound_packs_map_copy = sound_packs_map.clone();
+    let mut my_window = window::Window::new(100, 100, 350, 120, "rustyvibes -gui");
     let flex = group::Flex::default()
         .with_size(200, 100)
         .column()
         .center_of_parent();
-    let label = frame::Frame::default().with_label("Write down what you doing here ↓");
-    let mut input = input::MultilineInput::default();
-    input.set_value("");
+    let _label = frame::Frame::default().with_label("Select what kind you want ↓");
+    let mut choice = menu::Choice::default().with_size(60, 20).center_of_parent();
+    for item in &sound_packs {
+        choice.add_choice(item);
+    }
+    choice.set_value(1);
+    let mut btn_use = button::Button::default().with_label("Use It");
+    btn_use.set_callback(move |_| {
+        let index = choice.value().clone();
+        let a = sound_packs_copy.get(index as usize).unwrap();
+        print!("{:?}", a);
+
+        create_service(sound_packs_map_copy.get(a).unwrap().to_string());
+    });
     flex.end();
-    let mut button = button::Button::default()
-        .with_size(50, 30)
-        .with_pos(175, 220)
-        .with_label("Submit");
     my_window.end();
     my_window.show();
-
-  
     app.run().unwrap();
 }
 
-fn kill_service(handle: JoinHandle<()>) {
-    unsafe {
-        kill_thread_forcibly_exit_code(handle, 0);
-    }
-}
-
-fn create_service(sound_pack:String) -> JoinHandle<()>{
+fn create_service(sound_pack: String) -> JoinHandle<()> {
+    print!("{}", sound_pack);
     start::rustyvibes::start_rustyvibes(sound_pack, 100)
 }
